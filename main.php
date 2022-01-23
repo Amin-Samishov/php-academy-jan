63e92ef72af2aaee02c7efa2a76e5e3f50299560
Amin <amin.15.07.2005@mail.ru>
2022-01-23 14:59:58 +0500

    start cooking

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+Recipe for omelet with zucchini and shrimp
+Eggs - 3 pcs
+Milk - 150ml
+Zucchini - 1 pcs
+Shrimp - 100g
+oil - 50ml
+Salt - a couple
+*/
+
+function cookAnOmlet(float $eggs, float $milk, float $zucchini, float $shrimp, float $oil, float $salt)
+{
+    echo ""
+}
