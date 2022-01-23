a9960645f936a74dbf50a906709e2f6318823274
Amin <amin.15.07.2005@mail.ru>
2022-01-23 15:39:37 +0500

    start eating

--- main.php
+++ main.php
@@ -13,7 +13,8 @@ function cookAnOmlet(int $eggs, int $milk, int $zucchini, int $shrimp, int $oil,
 {
     echo "<br>fry the shrimp until almost cooked, <br> add zucchini and fry for a few minutes,<br> mix the eggs and milk,<br> salt and add to the shrimp <br> wait 5 minutes";
     /*
-
-    */
+pour the oil into the pan, wash the shrimp and fry until tender, stirring, cut the zucchini and add to the shrimp, 
+fry for 2-3 minutes, mix the eggs and milk, salt and pour the shrimp and wait 5 minutes. Bon Appetit    */
 }
-cookAnOmlet(eggs: 3, milk: 150, zucchini: 1, shrimp:100, oil: 50, salt: 5)
+cookAnOmlet(eggs: 3, milk: 150, zucchini: 1, shrimp:100, oil: 50, salt: 5, horoshieSlova'biloVkuso');
+?>
